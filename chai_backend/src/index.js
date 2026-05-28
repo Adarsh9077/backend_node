2980258d151f01feccbc5388393ff0716bce6336
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-05-28 20:35:19 +0530

    middleware/promises

--- chai_backend/src/index.js
+++ chai_backend/src/index.js
@@ -1,25 +1,31 @@
-import express from "express";
 import connectDB from "./db/db_helper.js";
-
-const app = express();
-
+import { app } from "./app.js";
 console.log("MONGODB_URI:", process.env.MONGODB_URI);
 
 (async () => {
   console.log("\n->\tConnecting to MongoDB...");
   try {
-    await connectDB();
+    await connectDB()
+      .then(() => {
+        app.listen(process.env.PORT, () => {
+          console.log(`Server is running at port: `);
+        });
+      })
+      .catch((err) => {
+        console.log("Mongo db connection failed", err);
+      });
+
     app.on("error", (error) => {
       console.log("Error:->", error);
       throw error;
     });
-    app.listen(process.env.PORT, () => {
-      console.log(`App running on port:${process.env.PORT}`);
-    });
+    // app.listen(process.env.PORT, () => {
+    //   console.log(`App running on port:${process.env.PORT}`);
+    // });
   } catch (error) {
     console.error("Fhhhaaaaa Error", error);
     throw error;
   }
 })();
 
-// ! Lec_08 00:00 Custom api response and error handling
+// ! Lec_08 24:05 Custom api response and error handling
