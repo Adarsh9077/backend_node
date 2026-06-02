81de753d3f879c45b799c6cfb68c8312370a2b96
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-06-02 08:18:47 +0530

    bcrypt hashing password

--- chai_backend/src/models/user.model.js
+++ chai_backend/src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
@@ -47,4 +49,14 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 export const User = mongoose.model("User", userSchema);
+
+// ! Lec_09 37:20 User and video model with hooks and JWT
--- chai_backend/src/models/video.model.js
+++ chai_backend/src/models/video.model.js
@@ -1,5 +1,5 @@
-// ! Lec_09 20:10 User and video model with hooks and JWT
 import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
   {
@@ -32,5 +32,7 @@ const videoSchema = new Schema(
   },
   { timestamps: true }
 );
+
+videoSchema.plugin(mongooseAggregatePaginate);
 
 export const Video = mongoose.model("Video", videoSchema);
