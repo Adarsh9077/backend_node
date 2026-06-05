689e0ca10dff6be3fd6ea0e5167a6d1cead9b64b
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-06-06 00:09:03 +0530

    add cloudinary snippets

--- chai_backend/src/utils/cloudinary.js
+++ chai_backend/src/utils/cloudinary.js
@@ -1,5 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
+import { loadEnvFile } from "process";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -7,4 +8,19 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-//! Lec_10 How to upload file in backend
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!loadEnvFile) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("File was uploaded on cloudinary");
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
+//! Lec_10 26:40 How to upload file in backend
