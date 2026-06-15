82ae16de0463a03d98a8c10ad32a6a860654f7f7
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-06-15 08:17:10 +0530

    validation applied and save into server local path, next step to upload file into cloudinary

--- chai_backend/src/controllers/user.controller.js
+++ chai_backend/src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
 const registerUser = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: "ok",
@@ -14,7 +15,24 @@ const registerUser = asyncHandler(async (req, res) => {
   //? Check for user creation
   //? Return Response
   const { fullname, email, username, password } = req.body;
-  console.log("Email:-> ",email)
+  console.log("Email:-> ", email);
+  if (
+    [fullname, username, email, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+  const existingUser = User.findOne({ $or: [{ username }, { email }] });
+  if (existingUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverPath[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
+  }
 });
 
 export { registerUser };
+
+// ? 34:35 Lec_13 Logic building | Register controller
