6392273681e884b1127231fd29548639c1312357
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-06-16 07:12:30 +0530

    create register controller

--- chai_backend/src/models/user.model.js
+++ chai_backend/src/models/user.model.js
@@ -51,7 +51,7 @@ const userSchema = new mongoose.Schema(
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
@@ -82,4 +82,4 @@ userSchema.methods.generateRefreshToken = function () {
   );
 };
 
-export const  User = mongoose.model("User", userSchema);
+export const User = mongoose.model("User", userSchema);
