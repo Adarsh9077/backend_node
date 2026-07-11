2b112f3dee4bdcacdb25e6e081f73194eeabaaa9
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-07-11 07:28:26 +0530

    user channel profile controller completed

--- chai_backend/src/controllers/user.controller.js
+++ chai_backend/src/controllers/user.controller.js
@@ -303,9 +303,37 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
       $addFields: {
         subscribersCount: { $size: "$subscribers" },
         channelsSubscribedToCount: { $size: "$subscribedTo" },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullname: 1,
+        username: 1,
+        email: 1,
+        avatar: 1,
+        coverImage: 1,
+        isSubscribed: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
       },
     },
   ]);
+  console.log("Channel:-> " + channel);
+
+  if (!channel?.length) {
+    throw new ApiError(404, "Channel does not exists");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, channel, "User channel fetched successfully"));
 });
 
 export {
@@ -320,4 +348,4 @@ export {
   updateUserCoverImage,
 };
 
-// ? 25:20 Lec_19 Learn Mongodb aggregation pipelines | Backend with JS
+// ? 25:2 get 0 Lec_19 Learn Mongodb aggregation pipelines | Backend with JS
