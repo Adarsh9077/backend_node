459718c48f08f146b0e617fe431a8a77d6d78b31
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-07-31 23:47:48 +0530

    try catch in publish a video

--- chai_backend/src/controllers/video.controller.js
+++ chai_backend/src/controllers/video.controller.js
@@ -20,45 +20,50 @@ const publishAVideo = asyncHandler(async (req, res) => {
   console.log(
     `title -> ${title}\ndescription -> ${description}\n- \t----------\t -\n`
   );
-  // console.log(req);
-  //   Todo: get video , upload on cloudinary, create video
-  let videoFileLocalPath /*= req.files?.videoFile[0]?.path*/;
 
-  if (req.files && req.files.videoFile && req.files.videoFile.length > 0) {
-    videoFileLocalPath = req.files.videoFile[0].path;
+  try {
+    let videoFileLocalPath;
+
+    if (req.files && req.files.videoFile && req.files.videoFile.length > 0) {
+      videoFileLocalPath = req.files.videoFile[0].path;
+    }
+    let thumbnailLocalPath /* = req.files?.thumbnail[0]?.path*/;
+
+    if (req.files && req.files.thumbnail && req.files.thumbnail.length > 0) {
+      thumbnailLocalPath = req.files.thumbnail[0].path;
+    }
+
+    console.log(`${videoFileLocalPath} <----> ${thumbnailLocalPath}`);
+
+    if (!videoFileLocalPath && !thumbnailLocalPath) {
+      throw new ApiError(400, "video file and thumbnail are required");
+    }
+
+    const videoFileLink = await uploadOnCloudinary(videoFileLocalPath);
+    const thumbnailLink = await uploadOnCloudinary(thumbnailLocalPath);
+
+    const videoObject = await Video.create({
+      videoFile: videoFileLink.url,
+      thumbnail: thumbnailLink.url,
+      title,
+      description,
+      duration: videoFileLink.duration,
+      owner: req.user._id,
+    });
+
+    const video = await Video.findById(videoObject._id).populate({
+      path: "owner",
+      select: "username email fullname avatar coverImage",
+    });
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, video, "video uploaded successfully"));
+  } catch (error) {
+    console.log("user not able to upload video on cloud");
+
+    return res.status(200).json(new ApiError(500, "Failed to upload "));
   }
-  let thumbnailLocalPath /* = req.files?.thumbnail[0]?.path*/;
-
-  if (req.files && req.files.thumbnail && req.files.thumbnail.length > 0) {
-    thumbnailLocalPath = req.files.thumbnail[0].path;
-  }
-
-  console.log(`${videoFileLocalPath} <----> ${thumbnailLocalPath}`);
-
-  if (!videoFileLocalPath && !thumbnailLocalPath) {
-    throw new ApiError(400, "video file and thumbnail are required");
-  }
-
-  const videoFileLink = await uploadOnCloudinary(videoFileLocalPath);
-  const thumbnailLink = await uploadOnCloudinary(thumbnailLocalPath);
-
-  const videoObject = await Video.create({
-    videoFile: videoFileLink.url,
-    thumbnail: thumbnailLink.url,
-    title,
-    description,
-    duration: videoFileLink.duration,
-    owner: req.user._id,
-  });
-
-  const video = await Video.findById(videoObject._id).populate({
-    path: "owner",
-    select: "username email fullname avatar coverImage",
-  });
-
-  return res
-    .status(200)
-    .json(new ApiResponse(200, video, "video uploaded successfully"));
 });
 
 const getVideoById = asyncHandler(async (req, res) => {
@@ -81,6 +86,10 @@ const deleteVideo = asyncHandler(async (req, res) => {
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
+  console.log(req.params);
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Delete a video controller"));
 });
 
 export {
