c91f3ae99b27c7c2a7badbb5784d6edaaf76eb8b
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-08-17 23:59:50 +0530

    comment controller

--- chai_backend/src/controllers/comment.controller.js
+++ chai_backend/src/controllers/comment.controller.js
@@ -1,13 +1,35 @@
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getVideoComments = asyncHandler(async (req, res) => {
   //Todo: get all comments for a video
   const { videoId } = req.params;
   const { page = 1, limit = 10 } = req.query;
+  try {
+  } catch (error) {}
 });
 
 const addComment = asyncHandler(async (req, res) => {
   //Todo: add a comment to a video
+  const { videoId } = req.params;
+  const { content } = req.body;
+  const { userId } = req.user._id;
+  try {
+    if (!videoId || !content) {
+      return res
+        .status(401)
+        .json(new ApiError(401, {}, "video id and content are required"));
+    }
+    const commentObject = await Comment.create({
+      content: content,
+      video: videoId,
+      owner: userId,
+    }).populate({ path: "owner", select: "username fullname email avatar" });
+    return res
+      .status(201)
+      .json(new ApiResponse(201, commentObject, "comment add successfully"));
+  } catch (error) {}
 });
 
 const updateComment = asyncHandler(async (req, res) => {
